78e6791761b32d7bc45502c6a6201669ae561eb7
GorjanKostadinovski <96490632+GorjanKostadinovski@users.noreply.github.com>
2022-07-05 18:28:09 +0200

    Add files via upload

---
+++ PizzaAppOnion.Api/Controllers/OrderController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaAppOnion.Contracts.Services;
+using PizzaAppOnion.Contracts.ViewModels.Order;
+using PizzaAppOnion.Domain.Entities;
+using PizzApp.Models.Mappers;
+
+namespace PizzApp.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly IOrderService _orderService;
+        private readonly IPizzaService _pizzaService;
+
+        public OrderController(IOrderService orderService,IPizzaService pizzaService)
+        {
+            _orderService = orderService;
+            _pizzaService = pizzaService;
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<OrderViewModel> orderListViewModel = _orderService.GetAllOrders();
+            return View(orderListViewModel);
+            
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            ViewBag.Pizzas = _orderService.GetOrder(id).Pizzas;
+            OrderDetailsViewModel orderDetailsviewModel = _orderService.GetOrdersDetails(id);
+
+            return View(orderDetailsviewModel);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+           OrderDetailsViewModel order = _orderService.GetOrdersDetails(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id , OrderDetailsViewModel order)
+        {
+
+            _orderService.EditOrder(id, order);
+            
+            return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            OrderDetailsViewModel orderDelete = _orderService.GetOrdersDetails(id);
+            if (orderDelete == null)
+            {
+                return NotFound();
+            }
+            return View(orderDelete);
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(int id, OrderDetailsViewModel order)
+        {
+            _orderService.DeleteOrder(id,order);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult CreateOrder()
+        {
+            ViewBag.Pizzas = _pizzaService.GetAllPizzas();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateOrder( OrderDetailsViewModel order)
+        {
+
+            _orderService.CreateNew(order);
+            return RedirectToAction("Index");
+                
+        }
+
+        
+
+    }
+}
---
+++ PizzaAppOnion.Api/Controllers/PizzaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaAppOnion.Contracts.Services;
+using PizzaAppOnion.Contracts.ViewModels.Pizza;
+using PizzaAppOnion.Contracts.ViewModels.Shared;
+using PizzaAppOnion.Domain.Entities;
+using PizzApp.Models.Mappers;
+using System.Diagnostics;
+
+
+namespace PizzApp.Controllers
+{
+   
+    public class PizzaController : Controller
+    {
+        private readonly IPizzaService _pizzaService;
+
+        public PizzaController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<PizzaViewModel> pizzaListViewModel = _pizzaService.GetAllPizzas();
+
+            return View(pizzaListViewModel);
+
+        }
+        
+        public IActionResult Details(int id)
+        {
+            PizzaDetailsViewModel pizzaDetailsviewModel = _pizzaService.GetPizzaDetails(id);
+
+            if (pizzaDetailsviewModel is null)
+            {
+                return RedirectToAction("Error");
+            }
+
+            return View(pizzaDetailsviewModel);
+        }
+
+               [HttpGet]
+        public IActionResult CreatePizza()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePizza(PizzaDetailsViewModel pizza)
+        {
+            _pizzaService.CreateNew(pizza);
+            return View(pizza);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            PizzaDetailsViewModel pizza = _pizzaService.GetPizzaDetails(id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, PizzaDetailsViewModel pizza)
+        {
+            
+            _pizzaService.EditPizza(id, pizza);
+            return View(pizza);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            PizzaDetailsViewModel pizzaDelete = _pizzaService.GetPizzaDetails(id);
+
+            if (pizzaDelete == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizzaDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id,PizzaDetailsViewModel pizza)
+        {
+            
+            _pizzaService.GetAllPizzaToDelete(id,pizza);
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
